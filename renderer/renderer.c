c05f02f3863ccc108127fad168b0749e5daaa429
Rajeev Tapadia <rtapadia730@gmail.com>
2026-01-24 13:04:17 +0530

    Clean up by introducing struct Layout
    Signed-off-by: Rajeev Tapadia <rtapadia730@gmail.com>

--- renderer/renderer.c
+++ renderer/renderer.c
@@ -3,26 +3,36 @@
 #include <raylib.h>
 #include <sigmidi-renderer.h>
 
-static int white_per_octave;
-static int white_key_count;
-static int white_width;
-static int white_height;
-static int black_width;
-static int black_height;
-static int octave_count;
+struct Layout {
+    int octave_count;
 
-void init_renderer(int width, int height, const char *title, int octaves) {
+    int white_per_octave;
+    int white_key_count;
+    int white_width;
+    int white_height;
+
+    int black_width;
+    int black_height;
+
+    int offset_y;
+};
+
+static struct Layout layout;
+
+void init_renderer(int width, int height, const char *title, int octave_count) {
     InitWindow(width, height, title);
     SetTargetFPS(60);
 
-    octave_count = octaves;
-    white_per_octave = 7;
-    white_key_count = octave_count * white_per_octave;
-    white_width = width / white_key_count;
-    white_height = height / 8;
+    layout.octave_count = octave_count;
+    layout.white_per_octave = 7;
+    layout.white_key_count = octave_count * layout.white_per_octave;
+    layout.white_width = width / layout.white_key_count;
+    layout.white_height = height / 8;
 
-    black_width = white_width * 0.5;
-    black_height = height / 12;
+    layout.black_width = layout.white_width * 0.5;
+    layout.black_height = height / 12;
+
+    layout.offset_y = height - (height / 8);
 }
 
 void begin_drawing() {
@@ -31,31 +41,35 @@ void begin_drawing() {
 }
 
 static void draw_piano_roll() {
-    int h = GetScreenHeight();
-    int y = h - (h / 8);
+    int y = layout.offset_y;
 
-    for (int i = 0; i <= white_key_count; i++) {
-        DrawRectangle(i * white_width, y, white_width - 1, white_height, RAYWHITE);
-        DrawRectangleLines(i * white_width, y, white_width - 1, white_height, BLACK);
+    for (int i = 0; i <= layout.white_key_count; i++) {
+        int x = i * layout.white_width;
+        int w = layout.white_width - 1;
+        int h = layout.white_height;
+
+        DrawRectangle(x, y, w, h, RAYWHITE);
+        DrawRectangleLines(x, y, w, h, BLACK);
     }
 
     static int black_key_pattern[] = {1, 1, 0, 1, 1, 1, 0};
 
-    for (int octave = 0; octave < octave_count; octave++) {
-        int base_white_idx = octave * white_per_octave;
+    for (int octave = 0; octave < layout.octave_count; octave++) {
+        int base_white_idx = octave * layout.white_per_octave;
         for (int key = 0; key < 7; key++) {
             if (!black_key_pattern[key])
                 continue;
             int white_idx = base_white_idx + key;
-            int x = (white_idx + 1) * white_width - (black_width / 2);
+            int x = (white_idx + 1) * layout.white_width - (layout.black_width / 2);
 
-            DrawRectangle(x, y, black_width, black_height, BLACK);
+            DrawRectangle(x, y, layout.black_width, layout.black_height, BLACK);
         }
     }
 }
 
 void end_drawing() {
     draw_piano_roll();
+    DrawFPS(0, 0);
     EndDrawing();
 }
 
@@ -70,6 +84,4 @@ void draw_note(struct Note note) {
     int bar_h = bar_w;
 
     DrawRectangle(note.note * bar_w, h / 2, bar_w, bar_h, RED);
-
-    DrawFPS(0, 0);
 }
